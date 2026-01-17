f2ef5bf9c3f5f0a7f5269e2571ce2bdeb672ebe1
Chris <capozzic1@gmail.com>
2026-01-17 17:35:59 -0500

    add animations

--- components/hero.js
+++ components/hero.js
@@ -8,20 +8,22 @@ export default function Hero(props) {
     console.log(props)
     const header = props.introHeader.header;
     const listItems = props.introHeader.heroList.map((item, idx) => {
-       return <li key={idx}>{item}</li>
+        return <li key={idx}>{item}</li>
     })
     return (
         <Row>
             <Col className={styles.heroContainer}>
-            <Image src={props.introHeader.heroIcon} alt ="portfolio" width={100} height={100}/>
+                <Image src={props.introHeader.heroIcon} alt="portfolio" width={100} height={100} />
                 {/* <ImageHeader  fullWidthHeader={true} imgSrc={props.introHeader.heroIcon} header={header}/> */}
-                <Col xxl={{span: 4, offset: 1 }} xs={{span: 10, offset: 1 }} md={{span: 7, offset: 1}}>
-                <div>
-                <h2 className={styles.introHeader}>{props.introHeader.header}</h2>
-                <ul className={styles.heroList}>
-                    {listItems}
-                </ul>
-                </div>
+                <Col xxl={{ span: 4, offset: 1 }} xs={{ span: 10, offset: 1 }} md={{ span: 7, offset: 1 }}>
+                    <div>
+                        <h2 className={`${styles.introHeader} ${styles['fade-in']}`}>{props.introHeader.header}</h2>
+                        <ul className={`${styles.heroList} ${styles['fade-in']}`}>
+                            {listItems}
+                        </ul>
+                    </div>
+                    <div className={`${styles.orb} ${styles.orb1}`}></div>
+                    <div className={`${styles.orb} ${styles.orb2}`}></div>
                 </Col>
             </Col>
         </Row>
