4f18755491dbeb75a4e9247205938fcc6a4f7804
Chris <capozzic1@gmail.com>
2026-01-18 15:17:20 -0500

    responsiveness

--- components/project-card.js
+++ components/project-card.js
@@ -11,13 +11,13 @@ export default function ProjectCard(props) {
     return (
         <div className={styles.projectCard}>
             <Row className={styles.projectRow}>
-                <Col xl={{ span: 8, offset: 2 }} xs={{ span: 10, offset: 1 }}>
+                <Col  xxxl={{ span: 4, offset:4  }}  xl={{ span: 8, offset: 2 }} xs={{ span: 10, offset: 1 }}>
                     <div className={styles.cardContent}>
                         <Col xl={12} className={styles.projectContainer}>
                             {/* If items provided, render a simple grid for them (used for Work Experience) */}
                             {(
                                 <>
-                                    <Col xl={{ span: 6, offset: 1 }}>
+                                    <Col xxxl={{ span: 7  }} xl={{ span: 6, offset: 1 }}>
                                         <ImageHeader imgSrc={props.cardData.imgSrc} header={props.cardData.header} />
                                     </Col>
                                     <div className={styles.btnContainer}>
